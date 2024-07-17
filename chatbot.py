2b263ee3f1bad8769d619bbf33903cb554c1e754
Marco <marco23a@hotmail.com>
2024-07-16 21:23:38 -0300

    list and configure

---
+++ chatbot.py
@@ -0,0 +1,30 @@
+import google.generativeai as genai
+
+keyUSER = input("Insert your gemini key: ")
+
+api_key = keyUSER
+
+genai.set_api_key(api_key)
+
+genai.configure(api_key=api_key)
+
+def listModelsAI():
+    for m in genai.Model.list():
+        if 'generateContent' in m.supported_generation_methods:
+            print(m.name)
+    
+
+def configAI():
+    config = {
+        "candidate_temperatue": 1,
+        "temperature": 0.5,
+    }
+
+    seguranca = {
+        "Harassment": "BLOCK_NONE",
+        "Violence": "BLOCK_NONE",
+        "Hate speech": "BLOCK_NONE",
+        "Hate speech or offensive language": "BLOCK_ALL",
+    }
+
+
